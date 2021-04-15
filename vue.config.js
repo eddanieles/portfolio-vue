b4e6d41b239fc3d200395c0d1e48255eca32c13d
eddanieles <eddanieles@gmail.com>
2021-04-15 01:12:50 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ?
-        '/vue-emailjs-demo/' : '/',
+        '/portfolio-vue/' : '/',
     "transpileDependencies": [
         "vuetify"
     ]
